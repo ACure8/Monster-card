75ad266c54309f093d1948039aa21ce0ba5e3af0
ACure8 <alexisme952@gmail.com>
2025-04-10 14:40:56 +1200

    Add files via upload

--- AssessmentMonsterCard.py
+++ AssessmentMonsterCard.py
@@ -63,6 +63,7 @@ Cards = { #These are the cards that already exist in the catalogue
     }
 }
 
+
 selection = { #This will be used to make navagating easier
     "Add Card" : "add",
     "Search" : "search",
@@ -72,7 +73,9 @@ selection = { #This will be used to make navagating easier
 
 
 #A main screen to navagate the options, I also moved it to the top to remove the error of "local variable 'selection' referenced before assignment"
+#New error that if you don't go back to privious selection then exit you won't be able to end this program
 def main():
+    user_choice = ""
     user_choice = eg.buttonbox("What do you want to do?", "Main", choices = list(selection.keys())) #I got the keys of the dictonary and turned them into a list so the choices will show "Add card", "Search", etc
     if user_choice:
         if selection[user_choice] == "add":
@@ -83,37 +86,102 @@ def main():
             delete()
         elif selection[user_choice] == "cards":
             cards()
+        elif user_choice == None:
+            exit()
 
 #add new cards to the catalogue Once the user has added a card, the program should display the card that
 #has been added, and check with the user whether the details are correct, or if
 #they want to make a change.
 def add():
-    eg.msgbox("this is Add Card")
+    eg.msgbox("This is Add Card")
     name = eg.enterbox("What do you want to name the monster?", "Add Card")
+    while name.title() in Cards.keys() or name in Cards.keys():
+        eg.msgbox(f"{name} already exists, try again", "error")
+        name = eg.enterbox("What do you want to name the monster?", "Add Card")
     strength = eg.integerbox("How much strength do you want to give the monster?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) #made a limiter for highest and lowest
     speed = eg.integerbox("How much speed do you want to give the monster?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) 
     stealth = eg.integerbox("How much stealth do you want to give the monster?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) 
     cunning = eg.integerbox("How much cunning do you want to give the monster?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1)
-
-    user_choice = eg.buttonbox("Do you want to return to Main screen? or end this program(Exit)", "Add Card", choices = list(selection.keys))
+    new_dict = {
+        name : {
+            "Strength" : strength,
+            "Speed" : speed,
+            "Stealth" : stealth,
+            "Cunning" : cunning
+        }
+    }
+    while eg.buttonbox(f"Are you happy with this or do you want to make some changes?\n{new_dict}", "Add Card", ["Change", "Done"]) == "Change": #This will loop till you're satisfied though it's not really working right
+        new_name = eg.enterbox(f"What name do you want for the monster instead of {name}", "Add Card")
+        while new_name.title() in Cards.keys() or new_name in Cards.keys():
+            eg.msgbox(f"{new_name} already exists, try again", "error")
+            new_name = eg.enterbox(f"What name do you want for the monster instead of {name}", "Add Card")
+        new_strength = eg.integerbox(f"How much strength do you want to give the monster instead of {strength}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1)
+        new_speed = eg.integerbox(f"How much speed do you want to give the monster instead of {speed}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) 
+        new_stealth = eg.integerbox(f"How much stealth do you want to give the monster instead of {stealth}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) 
+        new_cunning = eg.integerbox(f"How much cunning do you want to give the monster instead of {cunning}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1)
+        Cards[new_name] : {
+            "Strength" : new_strength,
+            "Speed" : new_speed,
+            "Stealth" : new_stealth,
+            "Cunning" : new_cunning
+        }
+        if eg.buttonbox(f"Are you happy with this or do you want to make some changes?\n{new_dict}", "Add Card", ["Change", "Done"]) == "Done":
+            break
+    Cards.update(new_dict)
+    eg.msgbox(Cards)
+    user_choice = eg.buttonbox("Do you want to return to Main screen? or end this program(Exit)", "Add Card", ["Main", "Exit"])
     if user_choice == "Main":
         main()
+    elif user_choice == "Exit":
+        exit()
 
 #Allow the user to search the catalogue for an existing monster card, check that
 #the monster’s details are correct, and make changes if necessary.
 def search():
-    eg.msgbox("this is search")
+    eg.msgbox("This is Search")
+    card = eg.choicebox("Pick which one you want to see:", "Search", choices = list(Cards)) #cancel does not work
+    if card == None:
+        main()
+    elif card:
+        leave = eg.buttonbox(f"{card}:\n{Cards[card]}","Search", ["Change", "Main", "Exit"])
+    if leave == "Change":
+        new_name = eg.enterbox(f"What name do you want for the monster instead of {card}", "Add Card")
+        while new_name.title() in Cards.keys() or new_name in Cards.keys():
+            eg.msgbox(f"{new_name} already exists, try again", "error")
+            new_name = eg.enterbox(f"What name do you want for the monster instead of {card}", "Add Card")
+        new_strength = eg.integerbox(f"How much strength do you want to give the monster instead of {Cards[card]['Strength']}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1)
+        new_speed = eg.integerbox(f"How much speed do you want to give the monster instead of {Cards[card]['Speed']}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) 
+        new_stealth = eg.integerbox(f"How much stealth do you want to give the monster instead of {Cards[card]['Stealth']}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1) 
+        new_cunning = eg.integerbox(f"How much cunning do you want to give the monster instead of {Cards[card]['Cunning']}?(minimum is 1 and max is 25)", "Add Card", upperbound = 25, lowerbound = 1)
+        Cards[new_name] = {
+            "Strength" : new_strength,
+            "Speed" : new_speed,
+            "Stealth" : new_stealth,
+            "Cunning" : new_cunning
+        }
+        eg.msgbox(Cards)
+    elif leave == "Main":
+        main()
+    else:
+        exit()
 
 #Delete a monster card from the catalogue.
 def delete():
-    eg.msgbox("this is delete")
+    eg.msgbox("This is Delete Card")
+    while True:
+        delete_card = eg.choicebox("Pick which one you want to delete:", "Delete Card", choices = list(Cards)) #cancel does not work
+        if delete_card:
+            if eg.ccbox("Are you sure?", "Delete Card"):  # show a Continue/Cancel dialog
+                delete = Cards.pop(delete_card)
+                eg.msgbox(Cards)
+        else:
+            main()
 
 #Output the full monster card catalogue to Python Shell (to allow for it to be printed out).
 def cards():
-    eg.msgbox("this is Catalogue") #I'm tagging the code under so the error doesn't interupt the rest of the code
-#    words = ""
- #   for i in list(Cards.keys):
-  #      words += f"{i}:\n{Cards[i]["Strength"]}\n{Cards[i]["Speed"]}\n{Cards[i]["Stealth"]}\n{Cards[i]["Cunning"]}\n" #I got error here saying "f-string:unmached '[' SyntaxError"
-   # print(words)
+    eg.msgbox("This is Catalogue")
+    print(Cards)
+    eg.msgbox("Check python shell")
+    main()
 
 main() #this will start the program
